ca0d209734129f348b0bf92ed448bc6f179b4da6
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-01-14 10:58:55 -0500

    Add project files.

---
+++ src/Backend/PlanShare.Infrastructure/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace PlanShare.Infrastructure.Extensions;
+public static class ConfigurationExtensions
+{
+    public static string ConnectionString(this IConfiguration configuration) => configuration.GetConnectionString("Connection")!;
+
+    public static bool IsUnitTestEnviroment(this IConfiguration configuration)
+    {
+        _ = bool.TryParse(configuration.GetSection("InMemoryTests").Value, out bool inMemoryTests);
+
+        return inMemoryTests;
+    }
+}
