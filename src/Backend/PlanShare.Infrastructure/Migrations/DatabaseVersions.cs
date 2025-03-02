be4d01f8218949421eb7ad3839c86dce435b2b40
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-03-02 14:50:23 -0500

    feat: adding first migration

---
+++ src/Backend/PlanShare.Infrastructure/Migrations/DatabaseVersions.cs
@@ -0,0 +1,6 @@
+﻿namespace PlanShare.Infrastructure.Migrations;
+
+internal sealed class DatabaseVersions
+{
+    internal const long TABLE_REGISTER_USER = 1;
+}
---
+++ src/Backend/PlanShare.Infrastructure/Migrations/Versions/Version00000001.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace PlanShare.Infrastructure.Migrations.Versions;
+
+[Migration(DatabaseVersions.TABLE_REGISTER_USER, "Create table to save the user's information")]
+public class Version00000001 : ForwardOnlyMigration
+{
+    public override void Up()
+    {
+        Create.Table("Users")
+            .WithColumn("Id").AsGuid().PrimaryKey().NotNullable()
+            .WithColumn("Active").AsBoolean().NotNullable().WithDefaultValue(true)
+            .WithColumn("CreatedOn").AsDateTime().NotNullable()
+            .WithColumn("Name").AsString(255).NotNullable()
+            .WithColumn("Email").AsString(255).NotNullable()
+            .WithColumn("Password").AsString(2000).NotNullable();
+    }
+}
