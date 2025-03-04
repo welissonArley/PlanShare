ba0629f8ae298d0ee8907ce949868a3bc2ae5b5d
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-03-04 07:11:27 -0500

    feat: creating the app project in net maui

---
+++ src/Mobile/PlanShare.App/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PlanShare.App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
