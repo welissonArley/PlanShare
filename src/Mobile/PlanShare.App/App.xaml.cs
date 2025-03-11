8d3e598a5ac95f4b98600ad9c53c5b13b62aa0df
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-03-11 18:57:49 -0400

    removing files we dont need and imporoving a litte bit the code

--- src/Mobile/PlanShare.App/App.xaml.cs
+++ src/Mobile/PlanShare.App/App.xaml.cs
@@ -1,15 +1,14 @@
-﻿namespace PlanShare.App
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
-        }
+﻿namespace PlanShare.App;
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
     }
 }
--- src/Mobile/PlanShare.App/AppShell.xaml.cs
+++ src/Mobile/PlanShare.App/AppShell.xaml.cs
@@ -1,10 +1,9 @@
-﻿namespace PlanShare.App
+﻿namespace PlanShare.App;
+
+public partial class AppShell : Shell
 {
-    public partial class AppShell : Shell
+    public AppShell()
     {
-        public AppShell()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
