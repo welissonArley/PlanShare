97fdba42ec8f48122dbcc1f00b2a25ab068b0a1c
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-03-18 19:39:41 -0400

    improving fonts

---
+++ src/Mobile/PlanShare.App/Constants/FontFamily.cs
@@ -0,0 +1,10 @@
+namespace PlanShare.App.Constants;
+
+public class FontFamily
+{
+    public const string MAIN_FONT_REGULAR = "RalewayRegular";
+    public const string MAIN_FONT_BLACK = "RalewayBlack";
+    public const string MAIN_FONT_THIN = "RalewayThin";
+    public const string SECONDARY_FONT_REGULAR = "WorkSansRegular";
+    public const string SECONDARY_FONT_BLACK = "WorkSansBlack";
+}
--- src/Mobile/PlanShare.App/MauiProgram.cs
+++ src/Mobile/PlanShare.App/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace PlanShare.App;
+﻿using PlanShare.App.Constants;
+
+namespace PlanShare.App;
 
 public static class MauiProgram
 {
@@ -9,11 +11,11 @@ public static class MauiProgram
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
 			{
-				fonts.AddFont("Raleway-Black.ttf", "RalewayBlack");
-				fonts.AddFont("Raleway-Regular.ttf", "RalewayRegular");
-				fonts.AddFont("Raleway-Thin.ttf", "RalewayThin");
-				fonts.AddFont("WorkSans-Black.ttf", "WorkSansBlack");
-				fonts.AddFont("WorkSans-Regular.ttf", "WorkSansRegular");
+				fonts.AddFont("Raleway-Black.ttf", FontFamily.MAIN_FONT_BLACK);
+				fonts.AddFont("Raleway-Regular.ttf", FontFamily.MAIN_FONT_REGULAR);
+				fonts.AddFont("Raleway-Thin.ttf", FontFamily.MAIN_FONT_THIN);
+				fonts.AddFont("WorkSans-Black.ttf", FontFamily.SECONDARY_FONT_BLACK);
+				fonts.AddFont("WorkSans-Regular.ttf", FontFamily.SECONDARY_FONT_REGULAR);
 			});
 
 		return builder.Build();
