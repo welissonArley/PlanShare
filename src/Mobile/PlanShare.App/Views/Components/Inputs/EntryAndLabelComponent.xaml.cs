a777af0878249fe509255e9b24155494d1fc2d35
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-03-23 12:04:17 -0400

    feat: adding first component in the project

---
+++ src/Mobile/PlanShare.App/Views/Components/Inputs/EntryAndLabelComponent.xaml.cs
@@ -0,0 +1,9 @@
+namespace PlanShare.App.Views.Components.Inputs;
+
+public partial class EntryAndLabelComponent : ContentView
+{
+	public EntryAndLabelComponent()
+	{
+		InitializeComponent();
+	}
+}
