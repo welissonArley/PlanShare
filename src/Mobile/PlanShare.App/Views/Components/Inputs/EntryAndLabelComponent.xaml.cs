c30d4e542a21dbbb0d3c22fd0f291ccfcda0b618
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-03-23 12:50:26 -0400

    feat: adding bindable properties for component

--- src/Mobile/PlanShare.App/Views/Components/Inputs/EntryAndLabelComponent.xaml.cs
+++ src/Mobile/PlanShare.App/Views/Components/Inputs/EntryAndLabelComponent.xaml.cs
@@ -2,7 +2,20 @@ namespace PlanShare.App.Views.Components.Inputs;
 
 public partial class EntryAndLabelComponent : ContentView
 {
-	public EntryAndLabelComponent()
+	public static readonly BindableProperty TitleProperty = BindableProperty
+		.Create(nameof(Title), typeof(string), typeof(EntryAndLabelComponent), string.Empty);
+
+    public static readonly BindableProperty PlaceholderProperty = BindableProperty
+        .Create(nameof(Placeholder), typeof(string), typeof(EntryAndLabelComponent), string.Empty);
+
+    public static readonly BindableProperty KeyboardProperty = BindableProperty
+        .Create(nameof(Keyboard), typeof(Keyboard), typeof(EntryAndLabelComponent), Keyboard.Default);
+
+    public string Title { get => (string)GetValue(TitleProperty); set => SetValue(TitleProperty, value); }
+	public string Placeholder { get => (string)GetValue(PlaceholderProperty); set => SetValue(PlaceholderProperty, value); }
+    public Keyboard Keyboard { get => (Keyboard)GetValue(KeyboardProperty); set => SetValue(KeyboardProperty, value); }
+
+    public EntryAndLabelComponent()
 	{
 		InitializeComponent();
 	}
