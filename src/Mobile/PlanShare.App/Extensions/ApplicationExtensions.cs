ed52f69d3cc16a4d059fac5c7a657c05c8cbdf8a
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-04-01 18:39:45 -0400

    feat: getting colors for entry

---
+++ src/Mobile/PlanShare.App/Extensions/ApplicationExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace PlanShare.App.Extensions;
+
+public static class ApplicationExtensions
+{
+    public static bool IsLightMode(this Application application) => application.RequestedTheme == AppTheme.Light;
+
+    public static Color GetPrimaryColor(this Application application)
+    {
+        var isLightMode = application.IsLightMode();
+
+        var key = isLightMode ? "PRIMARY_COLOR_LIGHT" : "PRIMARY_COLOR_DARK";
+
+        return (Color)application!.Resources[key];
+    }
+
+    public static Color GetLineColor(this Application application)
+    {
+        var isLightMode = application.IsLightMode();
+
+        var key = isLightMode ? "LINES_COLOR_LIGHT" : "LINES_COLOR_DARK";
+
+        return (Color)application!.Resources[key];
+    }
+}
--- src/Mobile/PlanShare.App/Resources/Styles/Handlers/CustomEntryHandler.cs
+++ src/Mobile/PlanShare.App/Resources/Styles/Handlers/CustomEntryHandler.cs
@@ -1,12 +1,15 @@
-﻿namespace PlanShare.App.Resources.Styles.Handlers;
+﻿using PlanShare.App.Extensions;
+
+namespace PlanShare.App.Resources.Styles.Handlers;
 
 public class CustomEntryHandler
 {
     public static void Customize()
     {
-        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("PlanShareEntry", (handler, view) =>
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("PlanShareEntry", (handler, entry) =>
         {
-
+            var cursorColor = Application.Current!.GetPrimaryColor();
+            var lineColor = Application.Current!.GetLineColor();
         });
     }
 }
