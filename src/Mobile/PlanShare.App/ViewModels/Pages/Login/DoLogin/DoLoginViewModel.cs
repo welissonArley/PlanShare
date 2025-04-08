b28596f91afde7c6fc9bde85b0c03cf2c3a10871
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-04-08 16:56:08 -0400

    feat: adding view model for login page

---
+++ src/Mobile/PlanShare.App/ViewModels/Pages/Login/DoLogin/DoLoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace PlanShare.App.ViewModels.Pages.Login.DoLogin;
+
+public partial class DoLoginViewModel : ObservableObject
+{
+    [ObservableProperty]
+    public string texto;
+
+    [RelayCommand]
+    public async Task DoLogin()
+    {
+        var textoDigitado = Texto;
+
+        Texto = "Welisson Arley";
+    }
+}
--- src/Mobile/PlanShare.App/Views/Pages/Login/DoLogin/DoLoginPage.xaml.cs
+++ src/Mobile/PlanShare.App/Views/Pages/Login/DoLogin/DoLoginPage.xaml.cs
@@ -1,3 +1,5 @@
+using PlanShare.App.ViewModels.Pages.Login.DoLogin;
+
 namespace PlanShare.App.Views.Pages.Login.DoLogin;
 
 public partial class DoLoginPage : ContentPage
@@ -5,5 +7,7 @@ public partial class DoLoginPage : ContentPage
 	public DoLoginPage()
 	{
 		InitializeComponent();
-	}
+
+		BindingContext = new DoLoginViewModel();
+    }
 }
