8b703fc951d6db369d2a919ae899ce1b57be565b
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-04-10 19:05:13 -0400

    feat: connection register user account page

---
+++ src/Mobile/PlanShare.App/Models/UserRegisterAccount.cs
@@ -0,0 +1,8 @@
+﻿namespace PlanShare.App.Models;
+
+public class UserRegisterAccount
+{
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
--- src/Mobile/PlanShare.App/ViewModels/Pages/User/Register/RegisterUserAccountViewModel.cs
+++ src/Mobile/PlanShare.App/ViewModels/Pages/User/Register/RegisterUserAccountViewModel.cs
@@ -1,5 +1,29 @@
-﻿namespace PlanShare.App.ViewModels.Pages.User.Register;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using PlanShare.App.Models;
+using PlanShare.App.Navigation;
+
+namespace PlanShare.App.ViewModels.Pages.User.Register;
 
 public partial class RegisterUserAccountViewModel : ViewModelBase
 {
+    [ObservableProperty]
+    public UserRegisterAccount model;
+
+    private readonly INavigationService _navigationService;
+
+    public RegisterUserAccountViewModel(INavigationService navigationService)
+    {
+        Model = new UserRegisterAccount();
+
+        _navigationService = navigationService;
+    }
+
+    [RelayCommand]
+    public async Task RegisterAccount()
+    {
+    }
+
+    [RelayCommand]
+    public async Task GoToLogin() => await _navigationService.GoToAsync($"../{RoutePages.LOGIN_PAGE}");
 }
