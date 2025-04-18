6b3831177850ecadcad56c33e18a8e1d253d1688
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-04-18 13:03:43 -0400

    feat: adding component for sending info animation

---
+++ src/Mobile/PlanShare.App/Views/Components/StatusPage/AnimationSendingInformationComponent.xaml.cs
@@ -0,0 +1,9 @@
+namespace PlanShare.App.Views.Components.StatusPage;
+
+public partial class AnimationSendingInformationComponent : ContentView
+{
+	public AnimationSendingInformationComponent()
+	{
+		InitializeComponent();
+	}
+}
