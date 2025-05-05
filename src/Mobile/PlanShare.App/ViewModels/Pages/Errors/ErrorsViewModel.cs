1e8329fcac99564c92ae79f7a00584c7f066d360
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-05-05 14:21:52 -0400

    feat: adding texts for the error page

--- src/Mobile/PlanShare.App/ViewModels/Pages/Errors/ErrorsViewModel.cs
+++ src/Mobile/PlanShare.App/ViewModels/Pages/Errors/ErrorsViewModel.cs
@@ -1,7 +1,17 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using PlanShare.App.Navigation;
 
 namespace PlanShare.App.ViewModels.Pages.Errors;
 public partial class ErrorsViewModel : ObservableObject
 {
+    private readonly INavigationService _navigationService;
 
+    public ErrorsViewModel(INavigationService navigationService)
+    {
+        _navigationService = navigationService;
+    }
+
+    [RelayCommand]
+    public async Task Close() => await _navigationService.GoToAsync("..");
 }
