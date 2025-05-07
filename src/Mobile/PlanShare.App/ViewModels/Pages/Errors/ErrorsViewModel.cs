5137e871fa890c075ff6d53257c70b06bc0cf25d
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-05-06 20:15:08 -0400

    feat: using collectionview

--- src/Mobile/PlanShare.App/ViewModels/Pages/Errors/ErrorsViewModel.cs
+++ src/Mobile/PlanShare.App/ViewModels/Pages/Errors/ErrorsViewModel.cs
@@ -1,15 +1,26 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using PlanShare.App.Navigation;
+using System.Collections.ObjectModel;
 
 namespace PlanShare.App.ViewModels.Pages.Errors;
 public partial class ErrorsViewModel : ObservableObject
 {
     private readonly INavigationService _navigationService;
 
+    [ObservableProperty]
+    public ObservableCollection<string> errorsList;
+
     public ErrorsViewModel(INavigationService navigationService)
     {
         _navigationService = navigationService;
+
+        ErrorsList = new ObservableCollection<string>()
+        {
+            "Error 1: Invalid input",
+            "Error 2: Network connection lost",
+            "Error 3: File not found"
+        };
     }
 
     [RelayCommand]
