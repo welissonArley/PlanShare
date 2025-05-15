1588cc540e2ea06b48309eb7c4910a5b4655f37c
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-05-15 18:19:20 -0400

    feat: avatar view with pen icon

--- src/Mobile/PlanShare.App/ViewModels/Pages/User/Profile/UserProfileViewModel.cs
+++ src/Mobile/PlanShare.App/ViewModels/Pages/User/Profile/UserProfileViewModel.cs
@@ -1,5 +1,20 @@
-﻿namespace PlanShare.App.ViewModels.Pages.User.Profile;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using PlanShare.App.Data.Storage.Preferences.User;
+using PlanShare.App.Navigation;
+
+namespace PlanShare.App.ViewModels.Pages.User.Profile;
 
 public partial class UserProfileViewModel : ViewModelBase
 {
+    [ObservableProperty]
+    public string userName;
+
+    private readonly INavigationService _navigationService;
+
+    public UserProfileViewModel(IUserStorage userStorage, INavigationService navigationService)
+    {
+        UserName = userStorage.Get().Name;
+
+        _navigationService = navigationService;
+    }
 }
