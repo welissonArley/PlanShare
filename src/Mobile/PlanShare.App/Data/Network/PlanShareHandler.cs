84ffc1c3548fbd5faea9cd56b4cb81aed45a2732
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-06-27 20:35:09 -0400

    feat: sending access token with the request

--- src/Mobile/PlanShare.App/Data/Network/PlanShareHandler.cs
+++ src/Mobile/PlanShare.App/Data/Network/PlanShareHandler.cs
@@ -1,16 +1,34 @@
-﻿using System.Globalization;
+﻿using PlanShare.App.Data.Storage.SecureStorage.Tokens;
+using System.Globalization;
 using System.Net.Http.Headers;
 
 namespace PlanShare.App.Data.Network;
 public class PlanShareHandler : DelegatingHandler
 {
-    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    private readonly ITokensStorage _tokensStorage;
+
+    public PlanShareHandler(ITokensStorage tokensStorage)
+    {
+        _tokensStorage = tokensStorage;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        ChangeRequestCulture(request);
+
+        var tokens = await _tokensStorage.Get();
+
+        if(string.IsNullOrWhiteSpace(tokens.AccessToken) == false)
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", tokens.AccessToken);
+
+        return await base.SendAsync(request, cancellationToken);
+    }
+
+    private static void ChangeRequestCulture(HttpRequestMessage request)
     {
         var culture = CultureInfo.CurrentCulture.Name;
 
         request.Headers.AcceptLanguage.Clear();
         request.Headers.AcceptLanguage.Add(new StringWithQualityHeaderValue(culture));
-
-        return base.SendAsync(request, cancellationToken);
     }
 }
