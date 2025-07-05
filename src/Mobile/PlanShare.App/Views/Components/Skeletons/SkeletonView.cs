892d26d58d6fd81f3c7432b5348dc61e6fc65616
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-07-05 14:44:05 -0400

    feat: adding skeleton view component animation

--- src/Mobile/PlanShare.App/Views/Components/Skeletons/SkeletonView.cs
+++ src/Mobile/PlanShare.App/Views/Components/Skeletons/SkeletonView.cs
@@ -3,10 +3,24 @@
 namespace PlanShare.App.Views.Components.Skeletons;
 public class SkeletonView : BoxView
 {
+    private const uint ANIMATION_DURATION = 1500;
+    private const uint ANIMATION_RATE = 20;
+
     public SkeletonView()
     {
         Color = Application.Current!.GetSkeletonViewColor();
 
+        var smoothAnimation = new Animation();
 
+        smoothAnimation.WithConcurrent(f => Opacity = f, start: 0.3, end: 1, Easing.SinIn);
+        smoothAnimation.WithConcurrent(f => Opacity = f, start: 1, end: 0.3, Easing.SinInOut);
+
+        this.Animate(name: "FadeInOut",
+            animation: smoothAnimation,
+            rate: ANIMATION_RATE,
+            length: ANIMATION_DURATION,
+            easing: Easing.SinIn,
+            finished: null,
+            repeat: () => true);
     }
 }
