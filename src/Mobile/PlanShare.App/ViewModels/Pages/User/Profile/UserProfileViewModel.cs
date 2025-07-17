5dba5906fbbbd4a586df620c7cb4f1baf21d972f
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-07-17 13:45:01 -0400

    feat: implementing the code to show our poput

--- src/Mobile/PlanShare.App/ViewModels/Pages/User/Profile/UserProfileViewModel.cs
+++ src/Mobile/PlanShare.App/ViewModels/Pages/User/Profile/UserProfileViewModel.cs
@@ -1,9 +1,12 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Maui;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.Maui.Controls.Shapes;
 using PlanShare.App.Navigation;
 using PlanShare.App.Resources;
 using PlanShare.App.UseCases.User.Profile;
 using PlanShare.App.UseCases.User.Update;
+using PlanShare.App.ViewModels.Popups.Files;
 
 namespace PlanShare.App.ViewModels.Pages.User.Profile;
 
@@ -15,13 +18,18 @@ public partial class UserProfileViewModel : ViewModelBase
     private readonly IGetUserProfileUseCase _getUserProfileUseCase;
     private readonly IUpdateUserUseCase _updateUserUseCase;
 
+    private readonly IPopupService _popupService;
+
     public UserProfileViewModel(
         INavigationService navigationService,
         IGetUserProfileUseCase getUserProfileUseCase,
-        IUpdateUserUseCase updateUserUseCase) : base(navigationService)
+        IUpdateUserUseCase updateUserUseCase,
+        IPopupService popupService) : base(navigationService)
     {
         _getUserProfileUseCase = getUserProfileUseCase;
         _updateUserUseCase = updateUserUseCase;
+
+        _popupService = popupService;
     }
 
     [RelayCommand]
@@ -54,4 +62,20 @@ public partial class UserProfileViewModel : ViewModelBase
 
     [RelayCommand]
     public async Task ChangePassword() => await _navigationService.GoToAsync(RoutePages.USER_CHANGE_PASSWORD_PAGE);
+
+    [RelayCommand]
+    public async Task ChangeProfilePhoto()
+    {
+        var popupOptions = new PopupOptions
+        {
+            Shadow = null,
+            Shape = new RoundRectangle
+            {
+                CornerRadius = new CornerRadius(10),
+                StrokeThickness = 0
+            }
+        };
+
+        await _popupService.ShowPopupAsync<OptionsForProfilePhotoViewModel>(Shell.Current, popupOptions);
+    }
 }
