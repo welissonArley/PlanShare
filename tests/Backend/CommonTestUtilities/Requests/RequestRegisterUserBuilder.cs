99cafce00c75afd7fd8b3d59fdd310712a86672a
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-07-26 18:16:40 -0400

    feat: starting unit tests

---
+++ tests/Backend/CommonTestUtilities/Requests/RequestRegisterUserBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Bogus;
+using PlanShare.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+public class RequestRegisterUserBuilder
+{
+    public static RequestRegisterUserJson Build()
+    {
+        return new Faker<RequestRegisterUserJson>()
+            .RuleFor(user => user.Name, f => f.Person.FirstName)
+            .RuleFor(user => user.Email, (f, user) => f.Internet.Email(user.Name))
+            .RuleFor(user => user.Password, f => f.Internet.Password());
+    }
+}
