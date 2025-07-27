c6abec2c3be926ccd3ce6247b55927f3060912f6
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-07-27 16:59:51 -0400

    feat: finish unit tests for register user validator

--- tests/Backend/Validators.Tests/User/Register/RegisterUserValidatorTests.cs
+++ tests/Backend/Validators.Tests/User/Register/RegisterUserValidatorTests.cs
@@ -9,36 +9,106 @@ public class RegisterUserValidatorTests
     [Fact]
     public void Success()
     {
-        // Arrange
         var validator = new RegisterUserValidator();
 
         var request = RequestRegisterUserBuilder.Build();
 
-        // Act
         var result = validator.Validate(request);
 
-        // Assert
         result.IsValid.ShouldBeTrue();
     }
 
     [Fact]
     public void Error_Name_Empty()
     {
-        // Arrange
         var validator = new RegisterUserValidator();
 
         var request = RequestRegisterUserBuilder.Build();
         request.Name = string.Empty;
 
-        // Act
         var result = validator.Validate(request);
 
-        // Assert
         result.IsValid.ShouldBeFalse();
         result.Errors.ShouldSatisfyAllConditions(errors =>
         {
             errors.Count.ShouldBe(1);
             errors.ShouldContain(error => error.ErrorMessage.Equals(ResourceMessagesException.NAME_EMPTY));
+        });
+    }
+
+    [Fact]
+    public void Error_Email_Empty()
+    {
+        var validator = new RegisterUserValidator();
+
+        var request = RequestRegisterUserBuilder.Build();
+        request.Email = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(error => error.ErrorMessage.Equals(ResourceMessagesException.EMAIL_EMPTY));
+        });
+    }
+
+    [Fact]
+    public void Error_Email_Invalid()
+    {
+        var validator = new RegisterUserValidator();
+
+        var request = RequestRegisterUserBuilder.Build();
+        request.Email = "test.com";
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(error => error.ErrorMessage.Equals(ResourceMessagesException.EMAIL_INVALID));
+        });
+    }
+
+    [Fact]
+    public void Error_Password_Empty()
+    {
+        var validator = new RegisterUserValidator();
+
+        var request = RequestRegisterUserBuilder.Build();
+        request.Password = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(error => error.ErrorMessage.Equals(ResourceMessagesException.PASSWORD_EMPTY));
+        });
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(4)]
+    [InlineData(5)]
+    public void Error_Password_Invalid(int amountPasswordCharacters)
+    {
+        var validator = new RegisterUserValidator();
+
+        var request = RequestRegisterUserBuilder.Build(amountPasswordCharacters);
+
+        var result = validator.Validate(request);
+
+        result.IsValid.ShouldBeFalse();
+        result.Errors.ShouldSatisfyAllConditions(errors =>
+        {
+            errors.Count.ShouldBe(1);
+            errors.ShouldContain(error => error.ErrorMessage.Equals(ResourceMessagesException.INVALID_PASSWORD));
         });
     }
 }
