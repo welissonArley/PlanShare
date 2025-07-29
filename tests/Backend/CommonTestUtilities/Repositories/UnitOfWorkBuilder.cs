c3facf2d93e50b89a2c48b120f6a3443de702097
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-07-29 05:56:27 -0400

    feat: starting unit test usecase

---
+++ tests/Backend/CommonTestUtilities/Repositories/UnitOfWorkBuilder.cs
@@ -0,0 +1,8 @@
+﻿using Moq;
+using PlanShare.Domain.Repositories;
+
+namespace CommonTestUtilities.Repositories;
+public class UnitOfWorkBuilder
+{
+    public static IUnitOfWork Build() => new Mock<IUnitOfWork>().Object;
+}
---
+++ tests/Backend/CommonTestUtilities/Repositories/UserWriteOnlyRepositoryBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Moq;
+using PlanShare.Domain.Repositories.User;
+
+namespace CommonTestUtilities.Repositories;
+public class UserWriteOnlyRepositoryBuilder
+{
+    public static IUserWriteOnlyRepository Build()
+    {
+        var mock = new Mock<IUserWriteOnlyRepository>();
+
+        return mock.Object;
+    }
+}
