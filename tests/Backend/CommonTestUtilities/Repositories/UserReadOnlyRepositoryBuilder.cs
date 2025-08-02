899b7a7fa16bd9325ab2bd3638210d6b77e38356
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-08-02 15:06:40 -0400

    feat: moq for functions which return value

---
+++ tests/Backend/CommonTestUtilities/Repositories/UserReadOnlyRepositoryBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using PlanShare.Domain.Repositories.User;
+
+namespace CommonTestUtilities.Repositories;
+public class UserReadOnlyRepositoryBuilder
+{
+    private readonly Mock<IUserReadOnlyRepository> _mockRepository;
+
+    public UserReadOnlyRepositoryBuilder()
+    {
+        _mockRepository = new Mock<IUserReadOnlyRepository>();
+    }
+
+    public void ExistActiveUserWithEmail(string email)
+    {
+        _mockRepository.Setup(repository => repository.ExistActiveUserWithEmail(email))
+            .ReturnsAsync(true);
+    }
+
+    public IUserReadOnlyRepository Build() => _mockRepository.Object;
+}
---
+++ tests/Backend/UseCases.Tests/User/Register/RegisterUserUseCaseTests.cs
@@ -0,0 +1,22 @@
+﻿using CommonTestUtilities.Repositories;
+using PlanShare.Application.UseCases.User.Register;
+using PlanShare.Domain.Extensions;
+
+namespace UseCases.Tests.User.Register;
+public class RegisterUserUseCaseTests
+{
+    [Fact]
+    public async Task Success()
+    {
+        var useCase = CreateUseCase();
+    }
+
+    private RegisterUserUseCase CreateUseCase(string? emailAlreadyExist = null)
+    {
+        var userReadOnlyRepository = new UserReadOnlyRepositoryBuilder();
+        if(emailAlreadyExist.NotEmpty())
+            userReadOnlyRepository.ExistActiveUserWithEmail(emailAlreadyExist);
+
+        return new RegisterUserUseCase(null, null, userReadOnlyRepository.Build(), null, null);
+    }
+}
