5f96b7d0a9c36dabc8d6184542c538aed67f9037
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-08-04 06:19:38 -0400

    feat: adding tests for errors use case register user

--- tests/Backend/UseCases.Tests/User/Register/RegisterUserUseCaseTests.cs
+++ tests/Backend/UseCases.Tests/User/Register/RegisterUserUseCaseTests.cs
@@ -4,6 +4,8 @@ using CommonTestUtilities.Requests;
 using CommonTestUtilities.Security.Cryptography;
 using PlanShare.Application.UseCases.User.Register;
 using PlanShare.Domain.Extensions;
+using PlanShare.Exceptions;
+using PlanShare.Exceptions.ExceptionsBase;
 using Shouldly;
 
 namespace UseCases.Tests.User.Register;
@@ -20,6 +22,54 @@ public class RegisterUserUseCaseTests
 
         result.ShouldNotBeNull();
         result.Name.ShouldBe(request.Name);
+        result.Id.ShouldNotBe(Guid.Empty);
+        result.Tokens.ShouldNotBeNull();
+        result.Tokens.AccessToken.ShouldNotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task Error_EmailAlreadyRegistered()
+    {
+        var request = RequestRegisterUserBuilder.Build();
+
+        var useCase = CreateUseCase(request.Email);
+
+        var act = async () => await useCase.Execute(request);
+
+        var exception = await act.ShouldThrowAsync<ErrorOnValidationException>();
+
+        exception.ShouldSatisfyAllConditions(exception =>
+        {
+            exception.GetStatusCode().ShouldBe(System.Net.HttpStatusCode.BadRequest);
+            exception.GetErrorMessages().ShouldSatisfyAllConditions(erros =>
+            {
+                erros.Count.ShouldBe(1);
+                erros.ShouldContain(ResourceMessagesException.EMAIL_ALREADY_REGISTERED);
+            });
+        });
+    }
+
+    [Fact]
+    public async Task Error_Name_Empty()
+    {
+        var request = RequestRegisterUserBuilder.Build();
+        request.Name = string.Empty;
+
+        var useCase = CreateUseCase();
+
+        var act = async () => await useCase.Execute(request);
+
+        var exception = await act.ShouldThrowAsync<ErrorOnValidationException>();
+
+        exception.ShouldSatisfyAllConditions(exception =>
+        {
+            exception.GetStatusCode().ShouldBe(System.Net.HttpStatusCode.BadRequest);
+            exception.GetErrorMessages().ShouldSatisfyAllConditions(erros =>
+            {
+                erros.Count.ShouldBe(1);
+                erros.ShouldContain(ResourceMessagesException.NAME_EMPTY);
+            });
+        });
     }
 
     private RegisterUserUseCase CreateUseCase(string? emailAlreadyExist = null)
