ef33e7a4f3f1bbab0d18736b52584567e85a93cc
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-08-05 05:54:09 -0400

    tests: adding tests for usecase update user profile

---
+++ tests/Backend/CommonTestUtilities/Repositories/UserUpdateOnlyRepositoryBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Moq;
+using PlanShare.Domain.Entities;
+using PlanShare.Domain.Repositories.User;
+
+namespace CommonTestUtilities.Repositories;
+public class UserUpdateOnlyRepositoryBuilder
+{
+    public static IUserUpdateOnlyRepository Build(User user)
+    {
+        var mock = new Mock<IUserUpdateOnlyRepository>();
+
+        mock.Setup(repository => repository.GetById(user.Id)).ReturnsAsync(user);
+
+        return mock.Object;
+    }
+}
---
+++ tests/Backend/UseCases.Tests/User/Update/UpdateUserUseCaseTests.cs
@@ -0,0 +1,89 @@
+﻿using CommonTestUtilities.Entities;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using CommonTestUtilities.Services.LoggedUser;
+using PlanShare.Application.UseCases.User.Update;
+using PlanShare.Domain.Extensions;
+using PlanShare.Exceptions;
+using PlanShare.Exceptions.ExceptionsBase;
+using Shouldly;
+
+namespace UseCases.Tests.User.Update;
+public class UpdateUserUseCaseTests
+{
+    [Fact]
+    public async Task Success()
+    {
+        var user = UserBuilder.Build();
+        var request = RequestUpdateUserBuilder.Build();
+
+        var useCase = CreateUseCase(user);
+
+        var act = async () => await useCase.Execute(request);
+
+        await act.ShouldNotThrowAsync();
+
+        user.Name.ShouldBe(request.Name);
+        user.Email.ShouldBe(request.Email);
+    }
+
+    [Fact]
+    public async Task Error_EmailAlreadyRegistered()
+    {
+        var user = UserBuilder.Build();
+        var request = RequestUpdateUserBuilder.Build();
+
+        var useCase = CreateUseCase(user, request.Email);
+
+        var act = async () => await useCase.Execute(request);
+
+        var exception = await act.ShouldThrowAsync<ErrorOnValidationException>();
+
+        exception.ShouldSatisfyAllConditions(exception =>
+        {
+            exception.GetStatusCode().ShouldBe(System.Net.HttpStatusCode.BadRequest);
+            exception.GetErrorMessages().ShouldSatisfyAllConditions(erros =>
+            {
+                erros.Count.ShouldBe(1);
+                erros.ShouldContain(ResourceMessagesException.EMAIL_ALREADY_REGISTERED);
+            });
+        });
+    }
+
+    [Fact]
+    public async Task Error_Name_Empty()
+    {
+        var user = UserBuilder.Build();
+        var request = RequestUpdateUserBuilder.Build();
+        request.Name = string.Empty;
+
+        var useCase = CreateUseCase(user);
+
+        var act = async () => await useCase.Execute(request);
+
+        var exception = await act.ShouldThrowAsync<ErrorOnValidationException>();
+
+        exception.ShouldSatisfyAllConditions(exception =>
+        {
+            exception.GetStatusCode().ShouldBe(System.Net.HttpStatusCode.BadRequest);
+            exception.GetErrorMessages().ShouldSatisfyAllConditions(erros =>
+            {
+                erros.Count.ShouldBe(1);
+                erros.ShouldContain(ResourceMessagesException.NAME_EMPTY);
+            });
+        });
+    }
+
+    private UpdateUserUseCase CreateUseCase(PlanShare.Domain.Entities.User user, string? emailAlreadyExist = null)
+    {
+        var loggedUser = LoggedUserBuilder.Build(user);
+        var userUpdateOnlyRepository = UserUpdateOnlyRepositoryBuilder.Build(user);
+        var unitOfWork = UnitOfWorkBuilder.Build();
+
+        var userReadOnlyRepository = new UserReadOnlyRepositoryBuilder();
+        if (emailAlreadyExist.NotEmpty())
+            userReadOnlyRepository.ExistActiveUserWithEmail(emailAlreadyExist);
+
+        return new UpdateUserUseCase(loggedUser, userUpdateOnlyRepository, userReadOnlyRepository.Build(), unitOfWork);
+    }
+}
