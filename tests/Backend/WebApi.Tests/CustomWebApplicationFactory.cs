9952234c0b472cb1a90ec8cb8dbd90e946ff14bc
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-08-12 19:34:20 -0400

    tests: adding data to start memory database for integration tests

--- tests/Backend/WebApi.Tests/CustomWebApplicationFactory.cs
+++ tests/Backend/WebApi.Tests/CustomWebApplicationFactory.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using CommonTestUtilities.Entities;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using PlanShare.Domain.Dtos;
+using PlanShare.Domain.Security.Tokens;
 using PlanShare.Infrastructure.DataAccess;
+using WebApi.Tests.Resources;
 
 namespace WebApi.Tests;
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
+    public UserIdentityManager User;
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Tests")
@@ -19,6 +25,30 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
                     options.UseInMemoryDatabase("InMemoryDbForTesting");
                     options.UseInternalServiceProvider(provider);
                 });
+
+                using var scope = services.BuildServiceProvider().CreateScope();
+                
+                var dbContext = scope.ServiceProvider.GetRequiredService<PlanShareDbContext>();
+                dbContext.Database.EnsureDeleted();
+
+                var accessTokenGenerator = scope.ServiceProvider.GetRequiredService<IAccessTokenGenerator>();
+
+                StartDatabase(dbContext, accessTokenGenerator);
             });
+    }
+
+    private void StartDatabase(PlanShareDbContext dbContext, IAccessTokenGenerator accessTokenGenerator)
+    {
+        (var user, var password) = UserBuilder.Build();
+
+        dbContext.Users.Add(user);
+        dbContext.SaveChanges();
+
+        var tokensDto = new TokensDto()
+        {
+            Access = accessTokenGenerator.Generate(user).token
+        };
+
+        User = new UserIdentityManager(user, password, tokensDto);
     }
 }
---
+++ tests/Backend/WebApi.Tests/Resources/UserIdentityManager.cs
@@ -0,0 +1,20 @@
+﻿using PlanShare.Domain.Dtos;
+
+namespace WebApi.Tests.Resources;
+public class UserIdentityManager
+{
+    private readonly PlanShare.Domain.Entities.User _user;
+    private readonly string _password;
+    private readonly TokensDto _tokens;
+
+    public UserIdentityManager(PlanShare.Domain.Entities.User user, string password, TokensDto tokens)
+    {
+        _password = password;
+        _user = user;
+        _tokens = tokens;
+    }
+
+    public string GetPassword() => _password;
+    public string GetEmail() => _user.Email;
+    public string GetAccessToken() => _tokens.Access;
+}
