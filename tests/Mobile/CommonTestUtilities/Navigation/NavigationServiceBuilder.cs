bd2c0a1459541eca315f8822ba65c1102d7aaed0
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-08-20 17:28:21 -0400

    tests: starting tests for onboarding viewmodel

---
+++ tests/Mobile/CommonTestUtilities/Navigation/NavigationServiceBuilder.cs
@@ -0,0 +1,8 @@
+﻿using Moq;
+using PlanShare.App.Navigation;
+
+namespace CommonTestUtilities.Navigation;
+public class NavigationServiceBuilder
+{
+    public static Mock<INavigationService> Build() => new Mock<INavigationService>();
+}
---
+++ tests/Mobile/ViewModels.Tests/Pages/OnBording/OnBoardingViewModelTests.cs
@@ -0,0 +1,30 @@
+﻿using CommonTestUtilities.Navigation;
+using Moq;
+using PlanShare.App.Navigation;
+using PlanShare.App.ViewModels.Pages.OnBording;
+using Shouldly;
+
+namespace ViewModels.Tests.Pages.OnBording;
+public class OnBoardingViewModelTests
+{
+    [Fact]
+    public async Task Success_LoginWithEmailAndPassword()
+    {
+        var (viewModel, navigationService) = CreateViewModel();
+
+        var act = async () => await viewModel.LoginWithEmailAndPasswordCommand.ExecuteAsync(null);
+
+        await act.ShouldNotThrowAsync();
+
+        navigationService.Verify(service => service.GoToAsync(RoutePages.LOGIN_PAGE), Times.Once);
+    }
+
+    private (OnBoardingViewModel viewModel, Mock<INavigationService> navigationService) CreateViewModel()
+    {
+        var navigationService = NavigationServiceBuilder.Build();
+
+        var viewModel = new OnBoardingViewModel(navigationService.Object);
+
+        return (viewModel, navigationService);
+    }
+}
