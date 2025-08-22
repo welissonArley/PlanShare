2528f846c96738f4523ce94b2b99e85d46f42467
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-08-22 19:30:45 -0400

    tests: unit tests viewmodel register user part 1

---
+++ tests/Mobile/CommonTestUtilities/UseCases/User/Register/RegisterUserUseCaseBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Moq;
+using PlanShare.App.Models;
+using PlanShare.App.Models.ValueObjects;
+using PlanShare.App.UseCases.User.Register;
+
+namespace CommonTestUtilities.UseCases.User.Register;
+public class RegisterUserUseCaseBuilder
+{
+    public static IRegisterUserUseCase Build(Result result)
+    {
+        var mock = new Mock<IRegisterUserUseCase>();
+
+        mock.Setup(useCase => useCase.Execute(It.IsAny<UserRegisterAccount>()))
+            .ReturnsAsync(result);
+
+        return mock.Object;
+    }
+}
---
+++ tests/Mobile/ViewModels.Tests/Pages/User/Register/RegisterUserAccountViewModelTests.cs
@@ -0,0 +1,61 @@
+﻿using CommonTestUtilities.Navigation;
+using CommonTestUtilities.UseCases.User.Register;
+using Moq;
+using PlanShare.App.Models;
+using PlanShare.App.Models.ValueObjects;
+using PlanShare.App.Navigation;
+using PlanShare.App.ViewModels.Pages.User.Register;
+using Shouldly;
+
+namespace ViewModels.Tests.Pages.User.Register;
+public class RegisterUserAccountViewModelTests
+{
+    [Fact]
+    public async Task Success_GoToLogin()
+    {
+        var (viewModel, navigationService) = CreateViewModel(Result.Success());
+
+        var act = async () => await viewModel.GoToLoginCommand.ExecuteAsync(null);
+
+        await act.ShouldNotThrowAsync();
+
+        navigationService.Verify(service =>
+            service.GoToAsync(
+                It.Is<ShellNavigationState>(state =>
+                    state.Location.OriginalString.Equals($"../{RoutePages.LOGIN_PAGE}"))), Times.Once);
+    }
+
+    [Fact]
+    public async Task RegisterAccount_Executed_With_Valid_Result()
+    {
+        var (viewModel, navigationService) = CreateViewModel(Result.Success());
+
+        var act = async () => await viewModel.RegisterAccountCommand.ExecuteAsync(null);
+
+        await act.ShouldNotThrowAsync();
+
+        viewModel.StatusPage.ShouldBe(StatusPage.Default);
+
+        navigationService.Verify(service => service.GoToDashboardPage(), Times.Once);
+    }
+
+    [Fact]
+    public async Task RegisterAccount_Executed_With_Invalid_Result()
+    {
+        /*var (viewModel, navigationService) = CreateViewModel();
+
+        var act = async () => await viewModel.RegisterAccountCommand.ExecuteAsync(null);
+
+        await act.ShouldNotThrowAsync();*/
+    }
+
+    private (RegisterUserAccountViewModel viewModel, Mock<INavigationService> navigationService) CreateViewModel(Result result)
+    {
+        var navigationService = NavigationServiceBuilder.Build();
+        var useCase = RegisterUserUseCaseBuilder.Build(result);
+
+        var viewModel = new RegisterUserAccountViewModel(navigationService.Object, useCase);
+
+        return (viewModel, navigationService);
+    }
+}
