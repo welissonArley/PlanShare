36b0c138d166431369b0d44ad7062da77f1dab95
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-09-05 17:32:39 -0400

    feat: adding entity for refresh token

---
+++ src/Backend/PlanShare.Domain/Entities/RefreshToken.cs
@@ -0,0 +1,9 @@
+﻿namespace PlanShare.Domain.Entities;
+public class RefreshToken : EntityBase
+{
+    public string Token { get; set; } = string.Empty;
+    public Guid AccessTokenId { get; set; }
+    public Guid UserId { get; set; }
+
+    public User User { get; set; } = default!;
+}
