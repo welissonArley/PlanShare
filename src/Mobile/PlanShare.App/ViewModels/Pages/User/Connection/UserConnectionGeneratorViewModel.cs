d851d7ffaab96970cbb9b61dcbd4e48b92de2ab0
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-10-19 15:42:58 -0400

    feat: get connection code from hub and showing it

--- src/Mobile/PlanShare.App/ViewModels/Pages/User/Connection/UserConnectionGeneratorViewModel.cs
+++ src/Mobile/PlanShare.App/ViewModels/Pages/User/Connection/UserConnectionGeneratorViewModel.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.SignalR.Client;
 using PlanShare.App.Data.Network.Api;
 using PlanShare.App.Models;
 using PlanShare.App.Navigation;
+using PlanShare.App.UseCases.Authentication.Refresh;
+using PlanShare.Communication.Responses;
 
 namespace PlanShare.App.ViewModels.Pages.User.Connection;
 
@@ -12,13 +14,22 @@ public partial class UserConnectionGeneratorViewModel : ViewModelBase
     [ObservableProperty]
     public new ConnectionByCodeStatusPage statusPage;
 
+    [ObservableProperty]
+    public string connectionCode;
+
     private readonly HubConnection _connection;
 
+    // Codigo temporario
+    private readonly IUseRefreshTokenUseCase _useRefreshTokenUseCase;
+
     public UserConnectionGeneratorViewModel(
+        IUseRefreshTokenUseCase useRefreshTokenUseCase,
+
         IUserConnectionByCodeClient userConnectionByCodeClient,
         INavigationService navigationService) : base(navigationService)
     {
         _connection = userConnectionByCodeClient.CreateClient();
+        _useRefreshTokenUseCase = useRefreshTokenUseCase;
     }
 
     [RelayCommand]
@@ -26,6 +37,15 @@ public partial class UserConnectionGeneratorViewModel : ViewModelBase
     {
         StatusPage = ConnectionByCodeStatusPage.GeneratingCode;
 
+        // Codigo Temporario
+        await _useRefreshTokenUseCase.Execute();
+
         await _connection.StartAsync();
+
+        var result = await _connection.InvokeAsync<HubOperationResult<string>>("GenerateCode");
+
+        ConnectionCode = result.Response!;
+
+        StatusPage = ConnectionByCodeStatusPage.WaitingForJoiner;
     }
 }
